5310f69a356480ffdd51ed844804a82003ca9b35
Vinai Kopp <vinai@netzarbeiter.com>
2023-08-08 15:54:40 +0200

    feat: add new command options to generate a release

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-## Deprecated Mage-OS package repository mirror URL legacy argument name 
+## Deprecated Mage-OS package repository mirror URL legacy argument name
 ## for backward compatibility:  --mirror-base-url
 ## Instead, use the new option: --repo-base-url
 
@@ -11,15 +11,21 @@ self="$0"
 show_help () {
   cat >&2 << EOF
   Generate Mage-OS composer package repository
-  
+
   Usage:
     $self --target={build targets, comma seperated}  Mage-OS composer repository type to build (default: mirror)
           --repo-base-url=[https://repo.mage-os.org] The package repo base URL
-          
+
+          --mageosRelease=[1.0.0] The release version (only for build target "release")
+          --mageosVendor=[mage-os] The release composer vendor name (only for build target "release")
+          --upstreamRelease=[2.4.6-p1] The corresponding Magento Open Source release (only for build target "release")
+
   Possible target(s)
      mirror            Build Magento Open Source mirror repository
-     upstream-nightly  Build release based on the current Mage-OS development branch
-  
+     upstream-nightly  Build release based on the current Magento Open Source development branch
+     mageos-nightly    Build release based on the current Mage-OS development branch
+     release           Build Mage-OS release
+
 EOF
   exit 1
 }
@@ -27,29 +33,45 @@ EOF
 
 # Default build target
 TARGET=mirror
+VENDOR=mage-os
 
 while [[ $# -gt 0 ]]; do
   case $1 in
       --mirror-base-url=*|--repo-base-url=*)
       REPO_BASE_URL="${1#*=}"
       shift # argument + value
-      
+
       ;;
       --target=*)
       TARGET="${1#*=}"
       shift # argument + value
-      
+
+      ;;
+      --mageosRelease=*)
+      RELEASE="${1#*=}"
+      shift # argument + value
+
+      ;;
+      --mageosVendor=*)
+      VENDOR="${1#*=}"
+      shift # argument + value
+
+      ;;
+      --upstreamRelease=*)
+      UPSTREAM_RELEASE="${1#*=}"
+      shift # argument + value
+
       ;;
       --help|-h|help)
       show_help
       shift
-    
+
       ;;
       *)
       echo "Unknown command option: \"$1\"" >&2
       show_help
       shift
-      
+
       ;;
   esac
 done
@@ -72,28 +94,28 @@ echo "$TARGET" | grep -q "mageos-nightly" && {
   node src/make/mageos-nightly.js --outputDir="$OUT_DIR" --gitRepoDir="$GIT_REPO_DIR" --repoUrl="$REPO_BASE_URL"
 }
 
+echo "$TARGET" | grep -q "release" && {
+  node src/make/mageos-release.js --outputDir="$OUT_DIR" --gitRepoDir="$GIT_REPO_DIR" --repoUrl="$REPO_BASE_URL" \
+    --mageosRelease="$RELEASE"  --mageosVendor="$VENDOR" --upstreamRelease="$UPSTREAM_RELEASE"
+}
+
 echo Running satis...
 
 cd /satis
 
 # Set the repository homepage for the satis build config
 [[ -n "$REPO_BASE_URL" ]] && {
-  node /generate-repo/bin/set-satis-homepage-url.js --satisConfig=/generate-repo/satis.json --repoUrl="$REPO_BASE_URL" > /tmp/satis.json   
+  node /generate-repo/bin/set-satis-homepage-url.js --satisConfig=/generate-repo/satis.json --repoUrl="$REPO_BASE_URL" > /tmp/satis.json
 }
 
 [[ -z "$REPO_BASE_URL" ]] && {
-  cp /generate-repo/satis.json /tmp/satis.json   
+  cp /generate-repo/satis.json /tmp/satis.json
 }
 
 # Add generator version number to Satis HTML
 cp /generate-repo/mageos.html.twig ./views/mageos.html.twig
 
-GENERATOR_VERSION=$(cat /generate-repo/package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
-
+GENERATOR_VERSION=$(jq -r .version /generate-repo/package.json)
 printf "%s" "$GENERATOR_VERSION" > ./views/version
 
 # Build satis into /build
@@ -103,5 +125,5 @@ echo Setting url prefix in generated output to $REPO_BASE_URL...
 
 # Fix host prefix in generated output
 [[ -n "$REPO_BASE_URL" ]] && {
-  node /generate-repo/bin/set-satis-output-url-prefix.js --satisOutputDir=/build --repoUrl="$REPO_BASE_URL"  
+  node /generate-repo/bin/set-satis-output-url-prefix.js --satisOutputDir=/build --repoUrl="$REPO_BASE_URL"
 }
