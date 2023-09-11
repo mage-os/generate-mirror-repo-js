542e11192d136b31bd2ae477d084db39dc51559e
Vinai Kopp <vinai@netzarbeiter.com>
2023-09-11 15:41:46 +0200

    fix: tags with -dev suffix generate incompatible nightly version

--- tests/release-branch-build-tools.test.js
+++ tests/release-branch-build-tools.test.js
@@ -13,6 +13,7 @@ test('It calculates the version', () => {
   expect(sut.calcNightlyBuildPackageBaseVersion('103.0.2.1')).toBe('103.0.2.2');
   expect(sut.calcNightlyBuildPackageBaseVersion('v103.0.2.1')).toBe('v103.0.2.2');
   expect(sut.calcNightlyBuildPackageBaseVersion('0.4.0-beta1')).toBe('0.4.0.1-beta1');
+  expect(sut.calcNightlyBuildPackageBaseVersion('1.2.0-dev')).toBe('1.2.0.1-dev');
 });
 
 
@@ -22,6 +23,16 @@ test('It updates every version in the input package->version map', () => {
     'foo/bar': '103.0.2'
   }, '20220703')).toEqual({
     'foo/bar': '103.0.2.1-a20220703'
+  })
+  expect(sut.transformVersionsToNightlyBuildVersions({
+    'foo/bar': '1.2.0-dev'
+  }, '20220703')).toEqual({
+    'foo/bar': '1.2.0.1-dev+20220703'
+  })
+  expect(sut.transformVersionsToNightlyBuildVersions({
+    'foo/bar': '1.2.0-beta1'
+  }, '20220703')).toEqual({
+    'foo/bar': '1.2.0.1-beta120220703'
   })
   expect(sut.transformVersionsToNightlyBuildVersions({
     'foo/bar': '103.0.2',
